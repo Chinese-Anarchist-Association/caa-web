53f70d3169c896167bff3a8ab0d9d2838cbe3635
Hgnim <hgn1m@outlook.com>
2026-07-23 21:36:01 +0800

    将组织名添加至global语言文件和环境变量

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,12 +2,13 @@
 import {useTitle} from "@vueuse/core";
 import {autoUseI18n} from "@/utils/i18nUtils.ts";
 import {autoLoadLocale} from "@/ts/global/vue/autoLoadLocale.ts";
+import {name_enus} from "@/ts/env/stringEnv.ts";
 
 const {gt:t}=autoUseI18n();
 const lp:string="view_Home";
 
 autoLoadLocale(lp,()=>{
-  useTitle(t(`${lp}.title`));
+  useTitle(t(`global.name`));
 });
 </script>
 
@@ -16,8 +17,8 @@ autoLoadLocale(lp,()=>{
     <div class="container">
       <div class="row">
         <div class="col-12 text-center">
-          <h1>中国无政府主义协会</h1>
-          <h1>Chinese Anarchist Association</h1>
+          <h1>{{t(`global.name`)}}</h1>
+          <h1>{{name_enus}}</h1>
         </div>
       </div>
     </div>
