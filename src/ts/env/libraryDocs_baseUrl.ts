c61bdb366abfbcd88958714083213e8d35b81d32
Hgnim <hgn1m@outlook.com>
2026-08-04 18:42:54 +0800

    docsData的数据将可以存储至外部与远程，方便管理

--- src/ts/env/libraryDocs_baseUrl.ts
+++ src/ts/env/libraryDocs_baseUrl.ts
@@ -1,2 +1,10 @@
 //图书馆文档的基链接
-export const libraryDocs_baseUrl_1:string=import.meta.env.VITE_LIBRARYDOCS_BASEURL_1;
+//export const libraryDocs_baseUrl_1:string=import.meta.env.VITE_LIBRARYDOCS_BASEURL_1;
+
+//图书馆文档的基链接
+export const libraryDocs_baseUrl:{
+    [key: string]:string,
+}={
+    libraryDocs_baseUrl_1:import.meta.env.VITE_LIBRARYDOCS_BASEURL_1 as string,
+}
+export default libraryDocs_baseUrl;
