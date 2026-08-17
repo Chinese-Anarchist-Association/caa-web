2ff85cadd75bd8489a8a2fca4d031fe488e6f196
Hgnim <hgn1m@outlook.com>
2026-08-17 23:36:16 +0800

    网络身份验证器更新

---
+++ src/utils/base1024Random.ts
@@ -0,0 +1,36 @@
+import anyBase from 'any-base';
+import {BASE1024, BASE64} from './base1024.ts';
+import seedrandom from 'seedrandom';
+
+/**
+ * 初始化
+ * @param seed 种子
+ */
+export default function (seed:string){
+    const sr=seedrandom(seed);
+    function getRandomNumber(min:number,max:number){
+        return Math.floor(sr() * (max - min + 1)) + min;
+    }
+    const BASE1024R:string=(()=>{
+        let output:string=BASE1024;
+        //打乱次数
+        const loopNum:number=getRandomNumber(10,200);
+        for (let i=0;i<loopNum;i++){
+            const start=getRandomNumber(0,output.length-1);
+            const end=getRandomNumber(start+1,output.length);
+
+            //提取的内容
+            const ext=output.slice(start,end);
+            //剩余的内容
+            const rem=output.slice(0,start)+output.slice(end);
+
+            output=rem+ext;
+        }
+        return output;
+    })();
+
+    const base64ToBase1024R=anyBase(BASE64,BASE1024R);
+    const base1024RToBase64=anyBase(BASE1024R,BASE64);
+
+    return {base64ToBase1024R,base1024RToBase64};
+}
--- src/views/OnlineSelfVerificationor/ts/OSV.ts
+++ src/views/OnlineSelfVerificationor/ts/OSV.ts
@@ -3,13 +3,25 @@ import {getFormatTimeWithTimezone} from "@/utils/date_timezone.ts";
 //import {decUint8ArrayToString, encStringToUint8Array} from "@/utils/cryptoUint8Array.ts";
 //import {base94Decode, base94Encode} from "@/utils/base94.ts";
 import {decryptString, encryptString} from "@/utils/cryptoString.ts";
-import {base1024ToBase64, base64ToBase1024} from "@/utils/base1024.ts";
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc';
+import base1024Random from "@/utils/base1024Random.ts";
+import seedrandom from "seedrandom";
+
+dayjs.extend(utc);
 
 export default function (){
     type DataType={
         d:string,//date
         e?:string,//extra
     };
+    const seed:string = dayjs.utc(new Date().toISOString()).format('YYYY-MM-DD_HH');
+    const {base64ToBase1024R,base1024RToBase64}=base1024Random(seed);
+
+    function getOTPW(){//one-time password
+        return `${seed}/${seedrandom(seed)()}`;
+    }
+
     //region 加密
     const encPw:Ref<string>=ref('');
     const encExtraCt:Ref<string>=ref('');
@@ -17,14 +29,12 @@ export default function (){
     const encCopyBtn_show:Ref<boolean>=ref(false);
 
     function encBtn_click(){
+        //console.log(dayjs.utc(new Date().toISOString()).format('YYYYMMDDHH'),dayjs(new Date().toISOString()).format('YYYYMMDDHH'),new Date().toISOString())
         encOutputContent.value = '';
         encCopyBtn_show.value=false;
 
-        const pw:string=encPw.value.trim();
-        if (pw==''){
-            encOutputContent.value = "加密失败，密钥不能为空。"
-            return;
-        }
+        const pw:string=`${encPw.value.trim()}::${getOTPW()}`;
+        //console.log(pw);
 
         const content:string=JSON.stringify((()=>{
             if (encExtraCt.value!=''){
@@ -39,7 +49,7 @@ export default function (){
             }
         })());
         //console.log(content)
-        const output:string = base64ToBase1024(encryptString(content,pw))//base94Encode(encStringToUint8Array(content,pw));//encryptString(content,pw);
+        const output:string = base64ToBase1024R(encryptString(content,pw))//base94Encode(encStringToUint8Array(content,pw));//encryptString(content,pw);
         encOutputContent.value = `${output}`;
 
         encCopyBtn_show.value=true;
@@ -58,23 +68,19 @@ export default function (){
     function decCopyBtn_click(){
         decOutputContent.value = '';
 
-        const pw:string=decPw.value.trim();
+        const pw:string=`${decPw.value.trim()}::${getOTPW()}`;
         const ipt:string = decInput.value;
         if (ipt==''){
             decOutputContent.value = '解密失败，密文不能为空';
             return;
         }
-        if (pw ==''){
-            decOutputContent.value = "解密失败，密钥不能为空";
-            return;
-        }
 
         try {
-            const content: DataType = JSON.parse(decryptString(base1024ToBase64(ipt),pw)/*decUint8ArrayToString(base94Decode(ipt), pw)*/);
+            const content: DataType = JSON.parse(decryptString(base1024RToBase64(ipt),pw)/*decUint8ArrayToString(base94Decode(ipt), pw)*/);
             decOutputContent.value =
                 `解密成功：\r\n时间：${getFormatTimeWithTimezone(content.d)}${(content.e) ? `\r\n附加内容：${content.e}` : ''}`;
         }catch{
-            decOutputContent.value = '解密失败，可能是密文或密钥错误';
+            decOutputContent.value = '解密失败，可能是密文或密钥错误，或密文已过期';
         }
     }
     //endregion
